97c5e6ea70f3504d6df2f9c7a99de6f8f34c7b70
Jeferson Tapia <jva.tapia@yavirac.edu.ec>
2022-07-15 16:16:31 -0500

    finalizacion de cambios

--- crm-backend/src/main/java/eduyaviracjef/crmbakendjef/feauter/person/PersonController.java
+++ crm-backend/src/main/java/eduyaviracjef/crmbakendjef/feauter/person/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin({"*"})
 @RequestMapping("/api/person")
 public class PersonController {
 
@@ -50,10 +52,9 @@ public class PersonController {
     
     //delete
     @DeleteMapping("/delete/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public String deleteById(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id){
             personService.deleteById(id);
-        return "registro eliminado";
     }
 
     @GetMapping("/all")
